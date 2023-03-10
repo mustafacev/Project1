5d5fbd7943235c776280ff814853dd72bb56b459
mustafacev <mustafaorcuncevik@gmail.com>
2023-03-09 21:11:17 -0500

    select cards and game board element

---
+++ app.js
@@ -0,0 +1,6 @@
+// Select Cards element
+const cards = document.querySelectorAll('.cardGame');
+//console.log(cards);
+// Select the game board element
+const gameBoard = document.querySelector('.game');
+//console.log(gameBoard)
